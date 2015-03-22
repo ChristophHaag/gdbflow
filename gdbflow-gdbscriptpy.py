703cf138ee4d7a930518d95433493d695ccd1901
Christoph Haag <haagch+git@frickel.club>
2015-03-22 13:43:42 +0100

    variables in each node

--- gdbflow-gdbscriptpy.py
+++ gdbflow-gdbscriptpy.py
@@ -2,9 +2,22 @@ import json
 gdb.execute("b _start")
 gdb.execute("b _exit")
 gdb.execute("r")
+
+def localvars(frame):
+  try:
+    block = gdb.block_for_pc(long(gdb.parse_and_eval('$pc')))
+  except: return None
+  symbols = []
+  for symbol in block:
+    s = {"name": symbol.print_name, "type": str(symbol.type)}
+    if symbol.is_variable:
+      s["value"] = str(symbol.value(frame))
+    symbols.append(s)
+  return symbols
+
 with open("gdbflow-log", "w") as f:
   while gdb.selected_inferior ().is_valid ():
-    print(str(gdb.selected_inferior ()), gdb.selected_inferior ().is_valid())
+    #print(str(gdb.selected_inferior ()), gdb.selected_inferior ().is_valid())
     gdb.execute("step")
     #sp = gdb.parse_and_eval("$sp")
     #pc = gdb.parse_and_eval("$pc")
@@ -19,6 +32,7 @@ with open("gdbflow-log", "w") as f:
     sal = frame.find_sal ()
     linerep["line"] = int(sal.line)
     linerep["filename"] = (sal.symtab.filename if sal.symtab else "")
+    linerep["vars"] = localvars(frame)
 
     f.write(json.dumps(linerep))
     f.write("\n")
--- gdbflow.py
+++ gdbflow.py
@@ -7,6 +7,7 @@ import pygraphviz as pgv
 parser = argparse.ArgumentParser(description='Flow visualization with gdb')
 parser.add_argument("-d", "--demo", help="visualize \"simple\" program", action="store_true", dest="demo")
 parser.add_argument('program', metavar='program', type=str, help='The program to debug', nargs="?")
+parser.add_argument("-q", "--quiet", help="don't show gdb output", dest="quiet", action="store_true")
 args = parser.parse_args()
 if not args.demo and not args.program:
     print("Error: Need a file name")
@@ -22,7 +23,12 @@ else:
     program = args.program
 
 print("Debugging program " + str(program))
-call(["gdb", "-batch-silent", "-q", program, "-x", "gdbflow-gdbscript"], stdout=PIPE, stderr=PIPE)
+if args.quiet:
+    print("Running gdb quietly")
+    call(["gdb", "-batch-silent", "-q", program, "-x", "gdbflow-gdbscript"], stdout=PIPE, stderr=PIPE)
+else:
+    print("Running gdb with output")
+    call(["gdb", program, "-x", "gdbflow-gdbscript"])
 
 G=pgv.AGraph()
 with open("gdbflow-log") as f:
@@ -31,7 +37,15 @@ with open("gdbflow-log") as f:
         #print(line)
         if not line.strip(): continue
         j = json.loads(line.strip())
-        nodename = str(index+1) + ": " + str(j["line"]) + " in " + j["funcname"] + " in " + j["filename"]
+        nodename = "Step " + str(index+1) + ":" + \
+                   "\nIn line " + str(j["line"]) + " in function " + j["funcname"] + " in file " + j["filename"]
+        if "vars" in j and j["vars"]:
+            for i in j["vars"]:
+                if not i: continue #no vars
+                nodename += "\nVariable name: " + str(i["name"])
+                nodename += "; Variable type: " + str(i["type"])
+                if "value" in i:
+                    nodename += "; Variable value: " + str(i["value"])
         G.add_node(nodename)
         G.add_edge(last,nodename)
         last = nodename
